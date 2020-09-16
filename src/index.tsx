2ab8fc4edd0a69ee3736717f91b71ff636293f3b
Maurício Nogueira de Oliveira <mauricio@flexpeak.com.br>
2020-09-16 07:55:34 -0400

    Criando telas

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import Dasboard from './pages/Dashboard/index';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Dasboard />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
