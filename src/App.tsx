d2e54b9dd7197992a8dc6906ccf7e0ebc681dc83
MauricioNogueira <mauricio.nogueiraa@gmail.com>
2020-09-27 18:56:55 -0400

    Adicionando tema light e dark

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import GlobalStyle from './styles/GlobalStyle';
-import Dashboard from './pages/Dashboard/index';
+// import Dashboard from './pages/Dashboard/index';
 import Layout from './components/Layout/index';
+
+import { ThemeProvider } from 'styled-components';
+import dark from './styles/themes/dark';
+// import light from './styles/themes/light';
 
 function App() {
     return (
-        <>
+        <ThemeProvider theme={dark}>
             <GlobalStyle />
             <Layout />
-        </>
+        </ThemeProvider>
     );
 }
 
